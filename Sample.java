0d9f9da7723fd23f041f40cadb338f103948c2b7
MDivya26 <divyadharsh560@gmail.com>
2026-06-23 14:27:48 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Sample{
+   int n1=1;
+   int n2=5;
+   int mul=n1*n2;
+   void display(){
+     System.out.println("Result:"+mul);
+    }
+ public static void main(String [] args){
+           Sample s=new Sample();
+           s.display();
+      }
+ }
+           
